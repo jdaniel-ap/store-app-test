9359b7291bb4b78a5014b0c36d0e4a63c1cd695f
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-12 18:56:24 -0300

    chore: setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.tsx';
 import { ErrorBoundary } from './components/layout';
@@ -9,13 +10,24 @@ import './i18n';
 import './index.css';
 import { Toaster } from 'sonner';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <Toaster />
+      </QueryClientProvider>
     </ErrorBoundary>
   </StrictMode>
 );
