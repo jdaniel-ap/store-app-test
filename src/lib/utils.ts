10ea6199d44a0f5b24d0c5cf9b433e01f72feda3
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-13 09:11:56 -0300

    refactor: util clx merge isntruction

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,12 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Combines class names using clsx and merges Tailwind classes to avoid conflicts
+ * @param inputs - Class names to combine
+ * @returns Merged class string
+ */
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export { queryKeys, type QueryKeys } from './queryKeys';
